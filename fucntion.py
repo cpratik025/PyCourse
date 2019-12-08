6b5662242cba2b1ecd7d0a28449c8e482ce759af
Pratik <cpratik025@gmail.com>
2019-12-08 12:39:44 -0500

    Updated Code

--- fucntion.py
+++ fucntion.py
@@ -16,3 +16,27 @@ say_something1()
 #Keyword Arguments, arguments and parameters are provided in the method itself
 
 say_something(name = 'Prratik')
+
+def say_nothing():
+    a = int(input("Please Enter a value"))
+    b = int(input("Please Enter a value"))
+    op= input("Please enter a operation")
+    if op == '+':
+        c= a+b
+        print(f"The summation of value is {c}")
+    elif op == '-':
+        c = a - b
+        print(f"The subtraction of value is {c}")
+    elif op == '/':
+        c = a/b
+        print(f"The value of division {c}")
+    elif op == '*':
+        c = a * b
+        print(f"The Multiplication of the values is {c}")
+    else:
+        print("The operation is incorrect")
+
+say_nothing()
+
+
+
